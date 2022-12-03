9c812bfd7cee8025e52993fa6fd6f0ae743671bc
oyoung <1019825864@qq.com>
2022-12-03 16:02:31 +0800

    fix: format

--- massa-oooooyoung.sh
+++ massa-oooooyoung.sh
@@ -9,6 +9,7 @@ Error="[${Red_font_prefix}错误${Font_color_suffix}]"
 Tip="[${Green_font_prefix}注意${Font_color_suffix}]"
 NODE_WALLET_PASSWORD="123456"
 
+
 check_root() {
     [[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限), 无法继续操作, 请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
 }
