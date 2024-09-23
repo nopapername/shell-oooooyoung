5018a180329b89f91372c66ab6b5e93f2a3cd66d
oy <1019825864@qq.com>
2024-09-23 11:00:45 +0800

    update

--- shell-script/cat20-oooooyoung.sh
+++ shell-script/cat20-oooooyoung.sh
@@ -28,6 +28,7 @@ install_env_and_full_node() {
 
     git clone https://github.com/CATProtocol/cat-token-box
     cd cat-token-box
+    git switch openminter_v2
     sudo yarn install
     sudo yarn build
 
