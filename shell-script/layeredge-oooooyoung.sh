bbee2f6ccc113f3bd23db2dd3286078e4e170cc1
oyoung <1019825864@qq.com>
2025-03-22 09:56:33 +0800

    fix

--- shell-script/layeredge-oooooyoung.sh
+++ shell-script/layeredge-oooooyoung.sh
@@ -149,7 +149,7 @@ setup_env() {
 
     # 创建新的 .env 文件
     cat >$ENV_FILE <<EOF
-GRPC_URL=34.31.74.109:9090
+GRPC_URL=grpc.testnet.layeredge.io:9090
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
 ZK_PROVER_URL=http://127.0.0.1:3001
 API_REQUEST_TIMEOUT=100
